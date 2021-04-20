703fca7fc328da31c3495d156f7d85aa9a2b2043
Arthur <arthurutschgg@gmail.com>
2021-04-20 16:56:42 -0300

    finalizando aula 1

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,21 @@
-import express from "express"
+import express, { request, response } from "express"
 
 const app = express();
+
+// GET = buscas
+// POST = criação
+// PUT = Alteração
+// Delete = deletar
+// PATH = alterar uma informação especifica
+
+
+app.get("/", (request, response) => {
+    return response.json({
+    message : "Olá NLW 5",
+    });
+});
+app.post("/", (request, response) => {
+    return response.json({ message: "Usuário salvo com sucesso!"});
+});
 
 app.listen(3333, () => console.log("Server está rodando na porta 3333"))
